10d56a0fa1065ecbb78a4cfbcb1cadd72d64c6ef
Jeia <jeiairso@gmail.com>
2026-02-16 20:51:39 -0800

    resizing

--- src/App.js
+++ src/App.js
@@ -51,8 +51,8 @@ export default function Portfolio() {
             </div>
           </div>
           <div className="flex-shrink-0">
-            <div className="w-70 h-70 rounded-full overflow-hidden border-4 border-white shadow-xl">
-              <img src="/JeiaSo_Headshot.jpg" alt="Jeia So" className="w-full h-auto" />
+            <div className="w-64 h-64 rounded-full overflow-hidden border-4 border-white shadow-xl">
+              <img src="/JeiaSo_Headshot.jpg" alt="Jeia So" className="w-full h-full object-cover" />
             </div>
           </div>
         </div>
